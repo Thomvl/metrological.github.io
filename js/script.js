c534ad7cb246720683e1fd9b23ad87757606a2f4
Jasper Vermeulen <jaspervermeulen1@gmail.com>
2018-08-29 09:40:24 +0200

    fix video

--- js/script.js
+++ js/script.js
@@ -23,8 +23,25 @@ document.addEventListener( `DOMContentLoaded`, function() {
 } )
 
 function play() {
-  document.getElementById( 'overlay' ).style.display = 'block'
   document.body.style.overflow = 'hidden'
+
+  $( '#overlay' ).on( 'click', close )
+
+  var modal = $( '.video-wrapper' )
+  var b = $( window )
+  var w = modal.width()
+  var h = modal.height()
+  var bw = b.width()
+  var bh = b.height()
+
+  modal.css( {
+    'position': 'absolute'
+  , 'top': ( ( bh - h ) / 2 ) + b.scrollTop() + 'px'
+  , 'left': ( ( bw - w ) / 2 ) + 'px'
+  } )
+
+  $( '#overlay' ).css( { visibility: 'visible' } )
+
   var vid = document.getElementById( 'video' )
   vid.play()
   vid.onended = close
@@ -32,6 +49,7 @@ function play() {
 
 function close() {
   document.getElementById( 'video' ).load()
-  document.getElementById( 'overlay' ).style.display = 'none'
+  $( '#overlay' ).off( 'click' )
+  $( '#overlay' ).css( { visibility: 'hidden' } )
   document.body.style.overflow = 'visible'
 }
